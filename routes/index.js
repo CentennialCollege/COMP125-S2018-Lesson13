a87ef76d8ba3b05ea6e629482fb1a34a54107334
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-08-07 14:19:25 -0400

    added bootstrap support

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express Demo' 
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'About Express',
+    author: 'Tom Tsiliopoulos'
+ });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+    title: 'Contact Me',
+    authors: ['Tom', 'Mark', 'Luke', 'Jaden', 'John']
+ });
 });
 
 module.exports = router;
